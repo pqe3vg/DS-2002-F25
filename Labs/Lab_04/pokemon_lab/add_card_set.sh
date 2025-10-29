ad1c824fcd5e7e0eb2e30778f2c60104c44139fc
pqe3vg <pqe3vg@virginia.edu>
2025-10-29 19:13:23 -0400

    Update add_card_set.sh

--- Labs/Lab_04/pokemon_lab/add_card_set.sh
+++ Labs/Lab_04/pokemon_lab/add_card_set.sh
@@ -1,9 +1,16 @@
-#!/bin/bash
-read -p "Enter the TCG Card Set ID (e.g., base1, base4): " SET_ID
+# !/bin/bash/ env bash
+
+# Prompt user for the TCG set ID
+read -r -p "Enter the TCG Card Set ID (e.g., base1, base4): " SET_ID
+
 if [ -z "$SET_ID" ]; then
     echo "Error: Set ID cannot be empty." >&2
     exit 1
 fi
-echo "Fetching card data for set ID: $SET_ID "
-curl -s "https://api.pokemontcg.io/v2/cards?q=set.id:${SET_ID}" \
-    -o "card_set_lookup/${SET_ID}.json"
+
+echo "Fetching card data for set ID: $SET_ID... "
+
+#API call to get all cards in the set
+curl -s "https://api.pokemontcg.io/v2/cards?q=set.id:$SET_ID&pageSize=250" > card_set_lookup/"$SET_ID".json
+
+echo "Successfully saved data to card_set_lookup/$SET_ID.json"
